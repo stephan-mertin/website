bb8f0a844fd0e12844e0eff863445026b4d2e104
Darius3001 <darius.mertin@outlook.com>
2026-08-20 21:44:08 +0200

    first iteration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 
 export default defineConfig({
   site: "https://stephan-mertin.github.io",
   base: "/website",
-  integrations: [react()],
 });
